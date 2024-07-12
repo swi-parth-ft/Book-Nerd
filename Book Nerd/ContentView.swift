0f35a959f9b623d445f0f4aa6e51a3988f718f4b
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-12 11:12:31 -0400

    Add Sorting

--- Book Nerd/ContentView.swift
+++ Book Nerd/ContentView.swift
@@ -12,7 +12,10 @@ import SwiftData
 struct ContentView: View {
     
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+    @Query(sort: [
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author)
+    ]) var books: [Book]
     @State private var showingAddScreen = false
     
     var body: some View {
